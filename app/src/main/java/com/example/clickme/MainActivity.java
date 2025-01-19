6e6c2dd027e645ffecb3c54b1a247f352e918b5d
ng5134 <ng5134@bs.amalnet.k12.il>
2025-01-19 14:09:39 +0200

    משימה 1

---
+++ app/src/main/java/com/example/clickme/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.clickme;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    boolean isClicked = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.Click1);
+
+    }
+
+    public void go(View view)
+    {
+        btn.setText("Oh, yea, I’ve been clicked!");
+    }
+}
